6c5ded21bc480c1a21260dbd14d0b972f67d8435
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-10 13:38:56 +0100

    moved sidebar definitions into this bundle

---
+++ DependencyInjection/MassiveContactExtension.php
@@ -0,0 +1,47 @@
+<?php
+/*
+* This file is part of the Sulu CMS.
+*
+* (c) MASSIVE ART WebServices GmbH
+*
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+
+namespace Massive\Bundle\ContactBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages bundle configuration for massive contact bundle
+ */
+class MassiveContactExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        foreach ($container->getExtensions() as $name => $extension) {
+            if ($name === 'sulu_admin') {
+                $container->prependExtensionConfig(
+                    $name,
+                    array('widget_groups' => array('contact-detail' => array('mappings' => array('sulu-contact-main-account'))))
+                );
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ Widgets/Table.php
@@ -0,0 +1,51 @@
+<?php
+/*
+  * This file is part of the Sulu CMS.
+  *
+  * (c) MASSIVE ART WebServices GmbH
+  *
+  * This source file is subject to the MIT license that is bundled
+  * with this source code in the file LICENSE.
+  */
+
+namespace Massive\Bundle\ContactBundle\Widgets;
+
+use Sulu\Bundle\AdminBundle\Widgets\WidgetInterface;
+
+/**
+ * Widget to display a table
+ */
+class Table implements WidgetInterface
+{
+    /**
+     * return name of widget
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'table';
+    }
+
+    /**
+     * returns template name of widget
+     *
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return 'MassiveContactBundle:Widgets:table.html.twig';
+    }
+
+    /**
+     * returns data to render template
+     *
+     * @param array $options
+     * @return array
+     */
+    public function getData($options)
+    {
+        // TODO: fetch contact here - (options contains all request parameters)
+        return $options;
+    }
+}
